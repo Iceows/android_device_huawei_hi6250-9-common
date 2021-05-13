954ecef837a26ce9813c30a43a797c25b1fc56ef
A2L5E0X1 <alexed25@protonmail.com>
2021-05-13 19:45:45 +0200

    hi6250-9-common: import custom mkbootimg.mk for huawei's bootimage split
    Change-Id: I424688b0314b498e7b084a0a643346a9a970f51b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -31,6 +31,8 @@ TARGET_COPY_OUT_VENDOR := vendor
 TARGET_USERIMAGES_USE_EXT4 := true
 
 # Kernel
+BOARD_CUSTOM_BOOTIMG := true
+BOARD_CUSTOM_BOOTIMG_MK := $(COMMON_PATH)/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x00478000
 BOARD_KERNEL_CMDLINE := loglevel=4 coherent_pool=512K page_tracker=on slub_min_objects=12 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz
---
+++ mkbootimg.mk
@@ -0,0 +1,52 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+# Global Flags
+HW_MKBOOTIMG := $(TARGET_KERNEL_SOURCE)/tools/mkbootimg
+
+# kernel.img
+INTERNAL_CUSTOM_BOOTIMAGE_ARGS := --base $(BOARD_KERNEL_BASE) --pagesize $(BOARD_KERNEL_PAGESIZE) --kernel $(INSTALLED_KERNEL_TARGET) --ramdisk $(BUILT_RAMDISK_TARGET) --cmdline "$(BOARD_KERNEL_CMDLINE) buildvariant=userdebug"
+
+$(INSTALLED_BOOTIMAGE_TARGET): $(HW_MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(BOOTIMAGE_EXTRA_DEPS) $(INSTALLED_KERNEL_TARGET)
+	$(call pretty,"Target boot image: $@")
+	$(hide) $(HW_MKBOOTIMG) $(INTERNAL_CUSTOM_BOOTIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE))
+
+# recovery_ramdisk.img
+INTERNAL_CUSTOM_RECOVERYIMAGE_ARGS := --kernel /dev/null --ramdisk $(recovery_ramdisk) --cmdline "buildvariant=$(TARGET_BUILD_VARIANT)"
+BOARD_CUSTOM_RECOVERY_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100
+
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_ramdisk)
+	@echo "----- Making recovery image ------"
+	$(hide) $(MKBOOTIMG) $(INTERNAL_CUSTOM_RECOVERYIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_INTERNAL_RECOVERY_MKBOOTIMG_ARGS) --output $@
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE))
+
+# ramdisk-recovery_vendor.img
+INSTALLED_RECVENDOR_RAMDISK_TARGET := $(PRODUCT_OUT)/ramdisk-recovery_vendor.img
+INSTALLED_RECVENDOR_RAMDISK_PLACEHOLDER_TARGET := $(PRODUCT_OUT)/ramdisk-recovery_vendor
+
+$(INSTALLED_RECVENDOR_RAMDISK_PLACEHOLDER_TARGET):
+	$(hide) mkdir -p $@/vendor
+	$(hide) touch $@/vendor/placeholder
+
+$(INSTALLED_RECVENDOR_RAMDISK_TARGET): $(MKBOOTFS) $(MINIGZIP) $(INSTALLED_RECVENDOR_RAMDISK_PLACEHOLDER_TARGET)
+	$(call pretty,"Target recovery_vendor ramdisk image: $@")
+	$(hide) $(MKBOOTFS) -d $(PRODUCT_OUT) $(INSTALLED_RECVENDOR_RAMDISK_PLACEHOLDER_TARGET) | $(MINIGZIP) > $@
+
+# recovery_vendor.img
+INSTALLED_RECVENDORIMAGE_TARGET := $(PRODUCT_OUT)/recovery_vendor.img
+INTERNAL_CUSTOM_RECVENDORIMAGE_ARGS := --kernel /dev/null --ramdisk $(INSTALLED_RECVENDOR_RAMDISK_TARGET) --cmdline "buildvariant=$(TARGET_BUILD_VARIANT)"
+BOARD_CUSTOM_RECVENDOR_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100
+
+.PHONY: recoveryvendorimage
+recoveryvendorimage: $(INSTALLED_RECVENDORIMAGE_TARGET)
+
+$(INSTALLED_RECVENDORIMAGE_TARGET): $(HW_MKBOOTIMG) $(INSTALLED_RECVENDOR_RAMDISK_TARGET)
+	$(call pretty,"Target recovery_vendor image: $@")
+	$(hide) $(HW_MKBOOTIMG) $(INTERNAL_CUSTOM_RECVENDORIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_CUSTOM_RECVENDOR_MKBOOTIMG_ARGS) --output $@
+	$(hide) $(call assert-max-image-size,$@,$(call get-hash-image-max-size,$(BOARD_RECVENDORIMAGE_PARTITION_SIZE)))
