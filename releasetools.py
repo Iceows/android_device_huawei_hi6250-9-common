0eab7f354f993df697d968c3b7902efbb33bd3cf
A2L5E0X1 <alexed25@protonmail.com>
2021-12-26 20:23:14 +0100

    hi6250-9-common: block twrp from installing this rom
    * We don't want to support TWRP and it's data fuckups.
    
    Change-Id: I918ca6f349e7c9d2f652b37086970800651abe60

--- releasetools.py
+++ releasetools.py
@@ -1,21 +1,25 @@
 #!/bin/env python3
+
 #
 # Copyright (C) 2021 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# SPDX-License-Identifier: Apache-2.0
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# This releasetools extension blocks TWRP from installing this ROM.
+#
 
 import common
 import re
+
+def FullOTA_Assertions(info):
+  OTA_Assertions(info)
+  return
+
+def IncrementalOTA_Assertions(info):
+  OTA_Assertions(info)
+  return
 
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info)
@@ -31,6 +35,9 @@ def AddImage(info, dir, basename, dest):
   info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
 
+def OTA_Assertions(info):
+  info.script.AppendExtra('assert(getprop("ro.twrp.boot") == "" || abort("ERROR: TWRP detected! Please use provided Lineage Recovery! For more info, read t.me/hi6250_oss/12."););')
+  return
+
 def OTA_InstallEnd(info):
   AddImage(info, "RADIO", "recovery_vendor.img", "/dev/block/platform/hi_mci.0/by-name/recovery_vendor")
-  return
