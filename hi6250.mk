67fb8f488b45ff845e8cd3c0660ed819bd13d6b6
A2L5E0X1 <alexed25@protonmail.com>
2022-01-05 15:36:38 +0100

    hi6250-9-common: import fstab and recovery init
    Change-Id: I298bf07ff26a2c7c0eeaacb384fc9fb3a96ab387

--- hi6250.mk
+++ hi6250.mk
@@ -7,5 +7,11 @@
 ## Common Path
 COMMON_PATH := device/huawei/hi6250-9-common
 
+# Init
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/init/fstab.hi6250:$(TARGET_COPY_OUT_RAMDISK)/fstab.hi6250 \
+    $(COMMON_PATH)/configs/init/fstab.hi6250:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.hi6250 \
+    $(COMMON_PATH)/configs/init/init.recovery.hi6250.rc:$(TARGET_RECOVERY_OUT)/root/init.recovery.hi6250.rc
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(COMMON_PATH)
