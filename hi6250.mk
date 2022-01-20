7c491de615591d83d96eed9a431f0f31896b6ec2
A2L5E0X1 <alexed25@protonmail.com>
2022-01-20 23:09:16 +0100

    hi6250-9-common: copy vndk28 libunwindstack for libxcollie
    * 01-19 18:41:16.824   413   413 E vndksupport: Could not load /vendor/lib64/hw/keystore.hi6250.so from default namespace: dlopen failed: cannot locate symbol _ZN11unwindstack4MapsD2Ev referenced by /vendor/lib64/libxcollie.so....
    
    Change-Id: Ifee8d2c0c5d42e52acdc44a2aa1db4c5bff6f8a7

--- hi6250.mk
+++ hi6250.mk
@@ -247,6 +247,10 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
+# VNDK
+PRODUCT_PACKAGES += \
+    libunwindstack.vendor
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
