hi6250-5/android_device_huawei_hi6250-common

sepolicy: add file contexts for BOARD_ROOT_EXTRA_FOLDERS
Change-Id: I4591e83f134851be6b8cc91c2b670a1e23b59b10

BoardConfigCommon.mk
@@ -93,6 +93,9 @@ BOARD_ROOT_EXTRA_FOLDERS += \
     splash2 \
     version
 
+# SELinux
+BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+
 # Vendor
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_VENDOR := vendor

Iceows/android_device_huawei_hi6250-9-common

Change-Id: I1914e8399231f8d7a0b631d9994b74d8ed16be64

BoardConfigCommon.mk
@@ -74,5 +74,8 @@ BOARD_ROOT_EXTRA_FOLDERS += \
     splash2 \
     version
 
+# SELinux
+BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+
 # Vendor
 VENDOR_SECURITY_PATCH := 2022-01-01 # ANE-LGRP2-OVS 9.1.0.401