d289c3f08627f6a1ae67f551f1b0c6fc4a6269b0
A2L5E0X1 <alexed25@protonmail.com>
2022-09-16 22:15:50 +0200

    hi6250-9-common: build hisi audio service
    Change-Id: I487667b94d424914a474191e8f7f7abeac2dace5

--- hi6250.mk
+++ hi6250.mk
@@ -24,7 +24,7 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.audio.service_64 \
+    android.hardware.audio.service.hisi \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
