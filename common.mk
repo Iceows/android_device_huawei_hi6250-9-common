5a0ed519dd38ba896fb6ec646ff12ec45937f556
Raphael Mounier <mounierr07@gmail.com>
2023-12-25 18:32:23 +0100

    common: drm use only widevine

--- common.mk
+++ common.mk
@@ -79,10 +79,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
     
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.2.vendor
 
 # ConfigStore (only service)
