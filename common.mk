a5d3e661a7a6760bf26fade114bc1278bbb93427
Raphael Mounier <mounierr07@gmail.com>
2024-02-07 14:56:24 +0100

    common: remove aosp keymaster3  impl  and service
    Usee now huawei keymaster

--- common.mk
+++ common.mk
@@ -178,8 +178,7 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service \
+    android.hardware.keymaster@3.0
     libkeymaster3device
     
 # KeyStore wifi
