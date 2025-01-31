521912a85b33ad8158e690b1ea893e2ec813149f
Iceows <mounierr07@gmail.com>
2025-01-31 08:52:29 +0100

    hi6250-common: remove twrp check

--- tools/releasetools.py
+++ tools/releasetools.py
@@ -1,25 +1,21 @@
 #!/bin/env python3
-
 #
 # Copyright (C) 2021 The LineageOS Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
-# This releasetools extension blocks TWRP from installing this ROM.
-#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import common
 import re
-
-def FullOTA_Assertions(info):
-  OTA_Assertions(info)
-  return
-
-def IncrementalOTA_Assertions(info):
-  OTA_Assertions(info)
-  return
 
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info)
@@ -35,9 +31,15 @@ def AddImage(info, dir, basename, dest):
   info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
 
-def OTA_Assertions(info):
-  info.script.AppendExtra('assert(getprop("ro.twrp.boot") == "" || abort("ERROR: TWRP detected! Please use provided Lineage Recovery! For more info, read t.me/hi6250_oss/12."););')
-  return
+def ImageExists(info, dir, basename):
+  try:
+    info.input_zip.getinfo(dir + "/" + basename)
+    return True
+  except KeyError:
+    return False
 
 def OTA_InstallEnd(info):
-  AddImage(info, "RADIO", "recovery_vendor.img", "/dev/block/platform/hi_mci.0/by-name/recovery_vendor")
+  if ImageExists(info, "RADIO", "boot_ramdisk.img"):
+    AddImage(info, "RADIO", "boot_ramdisk.img", "/dev/block/by-name/ramdisk")
+  AddImage(info, "RADIO", "recovery_vendor.img", "/dev/block/by-name/recovery_vendor")
+  return
